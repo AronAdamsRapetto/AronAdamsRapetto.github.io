9d2fdf20d9c8cf9431869492f6c66ac09dc7288b
Aron Adams <aronrapetto@hotmail.com>
2022-11-27 16:23:01 -0300

    feat: tela de contato iniciada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Projects from './Pages/projects';
-import About from './Pages/about';
+import Projects from './Pages/Projects';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <Switch>
+      <Route path="/contact" component={ Contact }/>
       <Route path="/projects" component={ Projects } />
       <Route path="/" component={ About } />
     </Switch>
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,14 @@
+import Header from "../Components/Header";
+// import { useState } from "react";
+import Footer from "../Components/Footer";
+
+function Contact() {
+  return (
+    <div>
+      <Header />
+      <Footer isFixed={ true } />
+    </div>
+  )
+}
+
+export default Contact;
